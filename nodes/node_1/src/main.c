37745ec9d08517f9813d7447bf8973dbf5468570
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-09-15 20:21:54 +0200

    memory: add SRAM support

--- nodes/node_1/src/main.c
+++ nodes/node_1/src/main.c
@@ -1,11 +1,13 @@
 #include <stdbool.h>
 #include <stdio.h>
-
 #include "usart.h"
+#include "memory.h"
 
 int main()
 {
 
+    memory_init();
+    memory_test();
     FILE *uart0 = usart_init(9600);
     char buffer[256] = {0};
     while (true)
@@ -13,10 +15,6 @@ int main()
         printf("Listening...\n\r");
         fgets(buffer, 30, uart0);
         printf("received: %s\n\r", buffer);
-    }
 
-    while (1)
-    {
     }
-
 }
