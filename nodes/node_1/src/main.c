0827ae12cc848b9456da384d9c3e0d783fee43be
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-10-14 18:10:58 +0200

    oled: fix font derefrence from progmem

--- nodes/node_1/src/main.c
+++ nodes/node_1/src/main.c
@@ -1,15 +1,12 @@
 #include <assert.h>
 #include <avr/interrupt.h>
+#include <avr/signature.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <util/delay.h>
 
-// #include "adc.h"
-#include "fb.h"
-#include "memory.h"
-// #include "oled.h"
-// #include "timer.h"
+#include "adc.h"
+#include "oled.h"
 #include "usart.h"
 
 ISR(BADISR_vect)
@@ -21,20 +18,19 @@ int main()
 {
     FILE *uart = usart_init(9600);
     (void)uart;
-    // printf("__malloc_heap_start: %p\n", (void *)__malloc_heap_start);
-    // printf("__malloc_heap_end: %p\n", (void *)__malloc_heap_end);
 
-    memory_init();
-    memory_test();
-    // adc_init();
-
-    FILE *oled = fb_init();
+    FILE *oled = oled_init();
     (void)oled;
 
+    adc_init();
+
+    cli();
+    sei();
     while (true)
     {
-        // channel_values adc_values = adc_read();
-        // printf("[acd]\t0x%X\t0x%X\t0x%X\t0x%X\t\n", adc_values.channel[0], adc_values.channel[1], adc_values.channel[2], adc_values.channel[3]);
+        channel_values adc_values = adc_read();
+        fprintf(uart, "[adc] 0x%X 0x%X 0x%X 0x%X\n", adc_values.channel[0], adc_values.channel[1], adc_values.channel[2], adc_values.channel[3]);
+        fprintf(oled, "%X %X %X %X\r\n", adc_values.channel[0], adc_values.channel[1], adc_values.channel[2], adc_values.channel[3]);
         _delay_ms(200);
     }
 }
--- nodes/node_1/src/modules/oled.c
+++ nodes/node_1/src/modules/oled.c
@@ -1,107 +1,161 @@
 #include "oled.h"
+
+#include <assert.h>
+#include <stdbool.h>
+
 #include "fonts.h"
 #include "memory.h"
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <util/delay.h>
-
-static void oled_carriage_return();
-static void oled_reset_page_counter();
+static int oled_write_char(char character, FILE *fd __attribute__((unused)));
+static void oled_flush();
+static void oled_increment_page_index();
+static void oled_increment_segment_index();
 static void oled_reset_cursor();
-static void write_c(uint8_t command_byte);
-static volatile uint8_t *oled_cmd = (volatile uint8_t *)0x1000;
-static volatile uint8_t *oled_data = (volatile uint8_t *)0x1200;
+static void oled_set_page_counter(uint8_t page_index);
+static void oled_set_segment_counter(uint8_t segment_index);
+static void oled_write_c(uint8_t command_byte);
 
-void oled_flush()
-{
-    for (int i = 0; i < OLED_NUM_SEGMENTS_PER_PAGE * OLED_NUM_PAGES; i++)
-    {
-        *oled_data = 0;
-    }
-}
+static uint8_t s_oled_page_index = 0;
+static uint8_t s_oled_segment_index = 0;
+static volatile uint8_t *s_oled_cmd = (volatile uint8_t *)0x1000;
+static volatile uint8_t *s_oled_data = (volatile uint8_t *)0x1200;
 
-void oled_init()
+FILE *oled_init()
 {
     static bool initialized = false;
     if (initialized)
     {
-        return;
+        assert(!"oled is already initialized");
     }
     initialized = true;
     printf("[info]: initializing oled\n");
-
     memory_init();
-    memory_add_delay();
 
     // display off
-    write_c(0xae);
+    oled_write_c(0xae);
     // segment remap
-    write_c(0xa1);
+    oled_write_c(0xa1);
     // common pads hardware: alternative
-    write_c(0xda);
-    write_c(0x12);
+    oled_write_c(0xda);
+    oled_write_c(0x12);
     // common output scan direction:com63~com0
-    write_c(0xc8);
+    oled_write_c(0xc8);
     // multiplex ration mode:63
-    write_c(0xa8);
-    write_c(0x3f);
+    oled_write_c(0xa8);
+    oled_write_c(0x3f);
     // display divide ratio/osc. freq. mode
-    write_c(0xd5);
-    write_c(0x80);
+    oled_write_c(0xd5);
+    oled_write_c(0x80);
     // contrast control
-    write_c(0x81);
-    write_c(0x50);
+    oled_write_c(0x81);
+    oled_write_c(0x50);
     // set pre-charge period
-    write_c(0xd9);
-    write_c(0x21);
+    oled_write_c(0xd9);
+    oled_write_c(0x21);
     // Set Horizontal Memory Addressing Mode
-    write_c(0x20);
-    write_c(0x00);
-
-    oled_reset_cursor();
+    oled_write_c(0x20);
+    oled_write_c(0x00);
 
     // start reading at RAM address = 0;
-    write_c(0x40);
+    oled_write_c(0x40);
 
     // VCOM deselect level mode
-    write_c(0xdb);
-    write_c(0x30);
+    oled_write_c(0xdb);
+    oled_write_c(0x30);
     // master configuration
-    write_c(0xad);
-    write_c(0x00);
+    oled_write_c(0xad);
+    oled_write_c(0x00);
     // out follows RAM content
-    write_c(0xa4);
+    oled_write_c(0xa4);
     // set normal display
-    write_c(0xa6);
+    oled_write_c(0xa6);
     // display on
-    write_c(0xaf);
+    oled_write_c(0xaf);
 
+    oled_reset_cursor();
     oled_flush();
 
-    return;
+    return fdevopen(oled_write_char, NULL);
 }
 
-static void write_c(uint8_t command_byte)
+static int oled_write_char(char character, FILE *fd __attribute__((unused)))
 {
-    *oled_cmd = command_byte;
+    switch (character)
+    {
+    case '\n':
+        oled_increment_page_index();
+        break;
+    case '\r':
+        oled_set_segment_counter(0);
+        s_oled_segment_index = 0;
+        break;
+    case '\t':
+        for (int i = 0; i < 4; i++)
+        {
+            oled_write_char(' ', fd);
+        }
+        break;
+    default:
+        char8 c = fonts_get_char8(character);
+        for (int i = 0; i < 8; i++)
+        {
+            *s_oled_data = c.segments[i];
+            oled_increment_segment_index();
+        }
+    }
+    return 0;
 }
 
-static void oled_carriage_return()
+static void oled_increment_segment_index()
 {
-    write_c(0x21);
-    write_c(0);
-    write_c(127);
+    if (++s_oled_segment_index == OLED_NUM_SEGMENTS_PER_PAGE)
+    {
+        s_oled_segment_index = 0;
+        oled_increment_page_index();
+    }
+    oled_set_segment_counter(s_oled_segment_index);
 }
-static void oled_reset_page_counter()
+
+static void oled_increment_page_index()
 {
-    write_c(0x20);
-    write_c(0);
-    write_c(7);
+    if (++s_oled_page_index == OLED_NUM_PAGES)
+    {
+        s_oled_page_index = 0;
+    }
+    oled_set_page_counter(s_oled_page_index);
 }
 
 static void oled_reset_cursor()
 {
-    oled_carriage_return();
-    oled_reset_page_counter();
+    oled_set_segment_counter(0);
+    oled_set_page_counter(0);
+}
+
+static void oled_set_segment_counter(uint8_t segment_index)
+{
+    assert(segment_index < 128);
+    oled_write_c(0x21);
+    oled_write_c(segment_index);
+    oled_write_c(127);
+}
+
+static void oled_set_page_counter(uint8_t page_index)
+{
+    assert(page_index < 8);
+    oled_write_c(0x22);
+    oled_write_c(page_index);
+    oled_write_c(7);
+}
+
+static void oled_flush()
+{
+    for (int i = 0; i < OLED_NUM_SEGMENTS_PER_PAGE * OLED_NUM_PAGES; i++)
+    {
+        *s_oled_data = 0;
+    }
+}
+
+static void oled_write_c(uint8_t command_byte)
+{
+    *s_oled_cmd = command_byte;
 }
