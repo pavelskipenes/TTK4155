be770acd14aec55839f72fd8af1b6cdb7f632070
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-10-14 18:35:39 +0200

    timer: add ctc timer module

--- nodes/node_1/src/modules/include/timer.h
+++ nodes/node_1/src/modules/include/timer.h
@@ -1,3 +1,8 @@
 #pragma once
 
+#include <stdint.h>
+
 void timer_enable_perif_clock();
+
+void timer1_ctc(uint8_t interrupt_frequency);
+void timer0_ctc(uint8_t interrupt_frequency);
--- nodes/node_1/src/modules/timer.c
+++ nodes/node_1/src/modules/timer.c
@@ -1,5 +1,8 @@
 #include "timer.h"
 
+#include <avr/io.h>
+#include <stdint.h>
+
 #include "gpio.h"
 #include <avr/io.h>
 
@@ -11,4 +14,37 @@ void timer_enable_perif_clock()
         &PORTB,
     };
     gpio_direction(&pb0, GPIO_OUTPUT);
+}
+
+void timer1_ctc(uint8_t interrupt_frequency)
+{
+    // prescaler: 1024, Clear Timer on Compare Match (CTC) with OCR1A register
+    TCCR1B |= (1 << WGM12) | (1 << CS12) | (1 << CS10);
+
+    // output compare interrupt enable
+    TIMSK |= (1 << OCIE1A);
+    TIFR |= (1 << OCF1A);
+
+    uint16_t counter = (F_CPU / 1024) * interrupt_frequency;
+
+    OCR1AH = (uint8_t)(counter >> 8);
+    OCR1AL = (uint8_t)(counter);
+}
+
+void timer0_ctc(uint8_t interrupt_frequency)
+{
+    // note: no result when using external clock on PB0
+    // internal clock prescalar 1024
+    TCCR0 |= (1 << CS02) | (0 << CS01) | (1 << CS00);
+
+    // CTC mode, no port operation
+    TCCR0 |= (1 << WGM01);
+
+    // Output Compare interrupt enable on Timer 0
+    SREG |= (1 << OCIE0);
+    TIFR |= (1 << OCF0);
+    TIMSK |= (1 << OCIE0);
+
+    uint8_t counter = (uint8_t)((F_CPU / 1024) * interrupt_frequency);
+    OCR0 = counter;
 }
