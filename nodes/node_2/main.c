535fcf72aac762c1a39355b6fff6772b912ed11e
Pavel Skipenes <pavelgs@stud.ntnu.no>
2022-11-16 19:50:41 +0100

    node_2: init can

--- nodes/node_2/main.c
+++ nodes/node_2/main.c
@@ -8,45 +8,25 @@
 
 int main()
 {
-	SystemInit();
+    SystemInit();
 
-	WDT->WDT_MR = WDT_MR_WDDIS; //Disable Watchdog Timer
-	PMC->PMC_WPMR &= ~(PMC_WPMR_WPEN); // PMC enable
+    WDT->WDT_MR = WDT_MR_WDDIS;        // Disable Watchdog Timer
+    PMC->PMC_WPMR &= ~(PMC_WPMR_WPEN); // PMC enable
 
-	uint8_t can_rx = PIO_PA1A_CANRX0;
-	uint8_t can_tx = PIO_PA0A_CANTX0;
-	uint32_t can_br = 167; // calculated
-	uint32_t can_ph1 = 7;
-	uint32_t can_ph2 = 6;
-	uint32_t can_prop = 2;
-	
-	uint16_t msg_id = 0;
-	char data[8] = "1234567";
-	char msg_length = '8';
+    configure_uart();
+    // brp = 42
+    // phase_1 = 7
+    // phase_2 = 6
+    // propagation = 2
+    can_init_def_tx_rx_mb(0x165 | (41 << CAN_BR_BRP_Pos));
 
-	configure_uart();
-	printf("Hello World\n\r");
-	can_init_def_tx_rx_mb(can_br, can_ph1, can_ph2,can_prop);
-	
-	CAN_MESSAGE msg = {
-		.id = msg_id,
-		.data_length = msg_length,
-		.data = data,
-	};
-	
-	while (1)
-	{
-		
-		//can_send(&msg, 0);
-		while(can_receive(&msg, 0));
-		//printf("%s",msg.data);
-		
-		for (int i = 0; i < 8; i++)
-		{
-			printf("%d\n\r", msg.data[i]);
-		}
-	
-	}
-	
-	
+    CAN_MESSAGE msg;
+    while (1)
+    {
+        uint8_t status = can_receive(&msg, 0x00);
+        if (!status)
+        {
+            printf("[NODE 2]: message received: %s\n\r", msg.data);
+        }
+    }
 }
